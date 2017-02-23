e349c064ebf29164e840c1a2db5008c4ddcbd4f9
Amine Sagaama <amine.sagaama@gmail.com>
2017-02-23 17:43:37 +0100

    modify server.js

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 //  OpenShift sample Node application
 var express = require('express'),
-    fs      = require('fs'),
-    app     = express(),
-    eps     = require('ejs'),
-    morgan  = require('morgan');
+    fs = require('fs'),
+    app = express(),
+    eps = require('ejs'),
+    morgan = require('morgan');
 
-Object.assign=require('object-assign')
+Object.assign = require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
-app.use(express.static('resources'))
-app.use('/resources', express.static(__dirname + '/public'));
+
+app.use(express.static(__dirname + "/public"));
+app.use(app.router);
 
 
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
@@ -34,20 +35,20 @@ if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
         }
         // Provide UI label that excludes user id and pw
         mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-        mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+        mongoURL += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
 
     }
 }
 var db = null,
     dbDetails = new Object();
 
-var initDb = function(callback) {
+var initDb = function (callback) {
     if (mongoURL == null) return;
 
     var mongodb = require('mongodb');
     if (mongodb == null) return;
 
-    mongodb.connect(mongoURL, function(err, conn) {
+    mongodb.connect(mongoURL, function (err, conn) {
         if (err) {
             callback(err);
             return;
@@ -66,17 +67,18 @@ app.get('/', function (req, res) {
     // try to initialize the db on every request if it's not already
     // initialized.
     if (!db) {
-        initDb(function(err){});
+        initDb(function (err) {
+        });
     }
     if (db) {
         var col = db.collection('counts');
         // Create a document with request IP and current time of request
         col.insert({ip: req.ip, date: Date.now()});
-        col.count(function(err, count){
-            res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+        col.count(function (err, count) {
+            res.render('index.html', {pageCountMessage: count, dbInfo: dbDetails});
         });
     } else {
-        res.render('index.html', { pageCountMessage : null});
+        res.render('index.html', {pageCountMessage: null});
     }
 });
 
@@ -84,10 +86,11 @@ app.get('/pagecount', function (req, res) {
     // try to initialize the db on every request if it's not already
     // initialized.
     if (!db) {
-        initDb(function(err){});
+        initDb(function (err) {
+        });
     }
     if (db) {
-        db.collection('counts').count(function(err, count ){
+        db.collection('counts').count(function (err, count) {
             res.send('{ pageCount: ' + count + '}');
         });
     } else {
@@ -96,16 +99,16 @@ app.get('/pagecount', function (req, res) {
 });
 
 // error handling
-app.use(function(err, req, res, next){
+app.use(function (err, req, res, next) {
     console.error(err.stack);
     res.status(500).send('Something bad happened!');
 });
 
-initDb(function(err){
-    console.log('Error connecting to Mongo. Message:\n'+err);
+initDb(function (err) {
+    console.log('Error connecting to Mongo. Message:\n' + err);
 });
 
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
 
-module.exports = app ;
+module.exports = app;
