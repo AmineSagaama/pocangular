a2f6f3128d210f388e5b9953f7adf83b1ea51845
Amine Sagaama <amine.sagaama@gmail.com>
2017-02-23 17:18:22 +0100

    add public access

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
+app.use(express.static('resources'))
+app.use('/resources', express.static(__dirname + '/public'));
+
+
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
@@ -105,5 +109,3 @@ app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
 
 module.exports = app ;
-
-app.use('resources', express.static(__dirname + '/public'));
